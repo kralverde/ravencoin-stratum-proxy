27a77cb03a577f1d4a7a9e6268eb41bef2192d0f
kralverde <2b2tminecrafter2@gmail.com>
2022-05-17 14:26:16 -0400

    tweaks

--- run_stratum_proxy.py
+++ run_stratum_proxy.py
@@ -94,7 +94,7 @@ async def execute(this_port: int, node_url: str, node_username: str, node_passwo
                         print(json_obj.get('error'))
                         raise Exception()
                     
-                    version_hex: str = json_obj['result']['version']
+                    version_int: str = json_obj['result']['version']
                     height_int: int = json_obj['result']['height'] 
                     bits_hex: str = json_obj['result']['bits'] 
                     prev_hash_hex: str = json_obj['result']['previousblockhash']
@@ -184,7 +184,7 @@ async def execute(this_port: int, node_url: str, node_username: str, node_passwo
 
                         # Done create merkle & update txs
 
-                        state.header = bytes.fromhex(version_hex)[::-1].hex() + \
+                        state.header = version_int.to_bytes(4, 'little') + \
                                 bytes.fromhex(prev_hash_hex)[::-1].hex() + \
                                 merkle.hex() + \
                                 ts.to_bytes(4, 'little').hex() + \
@@ -234,7 +234,7 @@ async def execute(this_port: int, node_url: str, node_username: str, node_passwo
 
                         # Done create merkle & update txs
 
-                        state.header = bytes.fromhex(version_hex)[::-1].hex() + \
+                        state.header = version_int.to_bytes(4, 'little') + \
                                 bytes.fromhex(prev_hash_hex)[::-1].hex() + \
                                 merkle.hex() + \
                                 ts.to_bytes(4, 'little').hex() + \
