9f0f66826861817dfcc6393a68847fc4c6c0fcdc
kralverde <2b2tminecrafter2@gmail.com>
2022-05-17 09:14:34 -0400

    tweaks

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ class TransactionState:
                         b'\x02' + \
                             my_sats.to_bytes(8, 'little') + op_push(len(vout1)) + vout1 + \
                             bytes(8) + op_push(len(witness_commitment) + 1) + b'\x6A' + witness_commitment + \
-                        + bytes(4)
+                        bytes(4)
 
     def update_transactions(self, version:int, height:int, bits:bytes, ts:int, prev_hash:bytes, incoming_transactions, my_sats, witness_commitment):
         
