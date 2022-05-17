4da7758b5ebaf7f9ca5cee90c7ba90056faac241
kralverde <2b2tminecrafter2@gmail.com>
2022-05-16 21:59:34 -0400

    tweaks

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ class StratumSession(RPCSession):
                 async def handle_submit(*args):
                     worker, job_id, nonce_hex, header_hex, mixhash_hex = args
                     temp_block_a, temp_block_b = self.tx.partial_block()
-                    full_block = temp_block_a + bytes.fromhex(nonce_hex[2:] + mixhash_hex[2:]) + temp_block_b
+                    full_block = temp_block_a + bytes(reversed(bytes.fromhex(nonce_hex[2:]))) + bytes(reversed(bytes.fromhex(mixhash_hex[2:]))) + temp_block_b
                     full_block = len(full_block).to_bytes(4, 'big') + full_block
                     print(full_block.hex())
                     data = {
